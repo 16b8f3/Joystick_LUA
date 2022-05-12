84d6f787a66a693afbd7073a2eccd4435f2a5bc7
Gildo Alves <104049719+16b8f3@users.noreply.github.com>
2022-05-12 09:06:36 -0300

    Variaveis Globais de Altura e Largura

--- main.lua
+++ main.lua
@@ -1,2 +1,8 @@
 -- Ocultando barra de Status
 display.setStatusBar(display.HiddenStatusBar);
+
+-- Variavel Global de Largura
+_w = display.viewableContentWidth;
+-- Variavel Global de Altura
+_h = display.viewableContentHeight;
+
