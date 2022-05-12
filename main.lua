332a288ad40eca35135fa505da9f5966d78966b4
Gildo Alves <104049719+16b8f3@users.noreply.github.com>
2022-05-12 09:21:57 -0300

    As 5 Áreas

--- main.lua
+++ main.lua
@@ -6,3 +6,22 @@ _w = display.viewableContentWidth;
 -- Variavel Global de Altura
 _h = display.viewableContentHeight;
 
+-- Area 1
+local area1 = display.newRect( 0, 0, 320, 480 );
+area1:setFillColor(31/255, 130/255, 160/255);
+
+-- Area 2
+local area2 = display.newRect( _w, 0, 320, 480 );
+area2:setFillColor(242/255, 46/255, 92/255);
+
+-- Area 3
+local area3  = display.newRect( 0, _h, 320, 480 );
+area3:setFillColor(242/255, 229/255, 46/255);
+
+-- Area 4
+local area4  = display.newRect( _w, _h, 320, 480 );
+area4:setFillColor(166/255, 155/255, 7/255);
+
+-- Descanso
+local descanso = display.newRect( _w/2, _h/2, 100, 100 );
+descanso:setFillColor(22/255, 184/255, 243/255);
