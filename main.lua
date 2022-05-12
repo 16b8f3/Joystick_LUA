4db7ee069ca68238923caba5baed787e577ea9e5
Gildo Alves <104049719+16b8f3@users.noreply.github.com>
2022-05-12 10:17:08 -0300

    Move Diagonal

--- main.lua
+++ main.lua
@@ -31,3 +31,52 @@ local bola = display.newCircle( _w/2, _h/2, 18 );
 bola:setFillColor( 255/255,255/255,255/255 );
 bola.strokeWidth = 2;
 bola:setStrokeColor(22/255, 184/255, 243/255);
+
+local direcaoA = 0;
+local direcaoB = 0;
+
+local function moveDiagonal(posicao)
+	if ( ( posicao.x > 110 and posicao.x < 210 ) and ( posicao.y > 190 and posicao.y < 290 ) ) then
+		direcaoA = 0;
+		direcaoB = 0;
+	else
+		if (posicao.phase == "began") then
+			bola:setFillColor(22/255, 184/255, 243/255);
+			bola:setStrokeColor( 255/255,255/255,255/255 );
+			-- Area 1
+			if ( ( posicao.x > 0 and posicao.x < 160 ) and ( posicao.y > 0 and posicao.y < 240 ) ) then
+				direcaoA = -1;
+				direcaoB = -1.5;
+			-- Area 2
+			elseif ( ( posicao.x > 160 and posicao.x < 320 ) and ( posicao.y > 0 and posicao.y < 240 ) ) then
+				direcaoA = 1;
+				direcaoB = -1.5;
+			-- Area 3
+			elseif ( ( posicao.x > 0 and posicao.x < 160 ) and ( posicao.y > 240 and posicao.y < 480 ) ) then
+				direcaoA = -1;
+				direcaoB = 1.5;
+			-- Area 4
+			elseif ( ( posicao.x > 160 and posicao.x < 320 ) and ( posicao.y > 240 and posicao.y < 480 ) ) then
+				direcaoA = 1;
+				direcaoB = 1.5;
+			end
+		elseif ( posicao.phase == "ended" ) then
+			bola:setFillColor( 255/255,255/255,255/255 );
+			bola.strokeWidth = 2;
+			bola:setStrokeColor(22/255, 184/255, 243/255);
+			direcaoA = 0;
+			direcaoB = 0;
+		end
+	end
+end
+
+local function onEnterFrame( ev )
+	Runtime:addEventListener("touch", moveDiagonal);
+	if (direcaoA ~= 0) then
+		bola.x = bola.x + direcaoA;
+	end
+	if (direcaoB ~= 0) then
+		bola.y = bola.y + direcaoB;
+	end
+end
+Runtime:addEventListener("enterFrame", onEnterFrame);
